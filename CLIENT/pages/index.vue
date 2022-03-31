ca24fb3883ef58a321e085671e9e4a8221cdf88d
coffeequickly <jangwonseok.js@gmail.com>
2022-03-31 11:41:44 +0900

    경로 분기(테스트용 PHP API 파일, 클라이언트-SSR 화면)

---
+++ CLIENT/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <article>
+        <dl>
+            <dt>로그인 / 로그아웃</dt>
+            <dd>
+                <input type="text" placeholder="ID">
+                <input type="text" placeholder="PW">
+                <button type="button">로그인</button>
+                <button type="button">로그아웃</button>
+            </dd>
+        </dl>
+
+        <dl>
+            <dt>사용자 상태</dt>
+            <dd>
+                <p>아이디</p>
+                <p>이름</p>
+                <p>로그인 시간</p>
+                <p>User Agent</p>
+            </dd>
+        </dl>
+
+        <dl>
+            <dt>토큰</dt>
+            <dd>AccessToken</dd>
+            <dd>RefreshToken</dd>
+        </dl>
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
